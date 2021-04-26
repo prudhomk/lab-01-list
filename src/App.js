c297b05d6895be2ffdc2d46bad26cd9cc32f57f2
Kalan Prudhomme <cleaver9@gmail.com>
2021-04-26 13:34:04 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { Component } from 'react';
-import logo from './react-logo.svg';
 import './App.css';
 import React from 'react';
-
+import Header from './header';
 class Hero extends Component {
 
   render() {
     return (
-      <header>
-        Hello
-      </header>
-        <main>
-            Where Stuff goes
-        </main>
-      <footer>
-        Goodbye
-      </footer>
+      <Header/>
+     
+     
     
     );
   }
